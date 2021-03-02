9f6c06d4cefa69366593348490a0af697d19237b
Moataz Mohammady <moataz@wuilt.com>
2021-03-02 13:49:42 +0200

    wip

---
+++ frontity.settings.js
@@ -0,0 +1,40 @@
+const settings = {
+  name: "onlinedirectory",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
+  },
+  packages: [
+    {
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Arabic", "/category/arabic/"],
+            ["English", "/category/english/"],
+          ],
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "http://onlinedirectory.local/",
+        },
+      },
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
+
+export default settings;
