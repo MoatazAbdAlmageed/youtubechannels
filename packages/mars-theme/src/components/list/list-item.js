31acb6cd693742aeb663bfb4f1041e7f6c9dc1de
Moataz Mohammady <moataz@wuilt.com>
2021-03-03 14:06:22 +0200

    show all content

--- packages/mars-theme/src/components/list/list-item.js
+++ packages/mars-theme/src/components/list/list-item.js
@@ -10,7 +10,8 @@ import FeaturedMedia from "../featured-media";
  * - Author: name of author and published date
  * - FeaturedMedia: the featured image/video of the post
  */
-const Item = ({ state, item }) => {
+const Item = ({ state, item, libraries }) => {
+  const Html2React = libraries.html2react.Component;
   const author = state.source.author[item.author];
   const date = new Date(item.date);
 
@@ -35,10 +36,9 @@ const Item = ({ state, item }) => {
         <FeaturedMedia id={item.featured_media} />
       )}
 
-      {/* If the post has an excerpt (short summary text), we render it */}
-      {item.excerpt && (
-        <Excerpt dangerouslySetInnerHTML={{ __html: item.excerpt.rendered }} />
-      )}
+      <Content>
+        <Html2React html={item.content.rendered} />
+      </Content>
     </article>
   );
 };
@@ -72,4 +72,133 @@ const PublishDate = styled.span`
 const Excerpt = styled.div`
   line-height: 1.6em;
   color: rgba(12, 17, 43, 0.8);
+`;
+
+// TODO:make it reusable
+/**
+ * This component is the parent of the `content.rendered` HTML. We can use nested
+ * selectors to style that HTML.
+ */
+const Content = styled.div`
+  color: rgba(12, 17, 43, 0.8);
+  word-break: break-word;
+
+  * {
+    max-width: 100%;
+  }
+
+  p {
+    line-height: 1.6em;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  figure {
+    margin: 24px auto;
+    /* next line overrides an inline style of the figure element. */
+    width: 100% !important;
+
+    figcaption {
+      font-size: 0.7em;
+    }
+  }
+
+  iframe {
+    display: block;
+    margin: auto;
+  }
+
+  blockquote {
+    margin: 16px 0;
+    background-color: rgba(0, 0, 0, 0.1);
+    border-left: 4px solid hsl(188deg 51% 52%);
+    padding: 4px 16px;
+  }
+
+  a {
+    color: hsl(180deg 51% 52%);
+    text-decoration: none;
+  }
+  a:hover {
+    color: #929eaa;
+  }
+
+  /* Input fields styles */
+
+  input[type="text"],
+  input[type="email"],
+  input[type="url"],
+  input[type="tel"],
+  input[type="number"],
+  input[type="date"],
+  textarea,
+  select {
+    display: block;
+    padding: 6px 12px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
+    outline-color: transparent;
+    transition: outline-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    margin: 8px 0 4px 0;
+
+    &:focus {
+      outline-color: hsl(180deg 51% 52%);
+    }
+  }
+
+  input[type="submit"] {
+    display: inline-block;
+    margin-bottom: 0;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    background-image: none;
+    border: 1px solid hsl(180deg 51% 52%);
+    padding: 12px 36px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    border-radius: 4px;
+    color: #fff;
+    background-color: hsl(180deg 51% 52%);
+  }
+
+  /* WordPress Core Align Classes */
+
+  @media (min-width: 420px) {
+    img.aligncenter,
+    img.alignleft,
+    img.alignright {
+      width: auto;
+    }
+
+    .aligncenter {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .alignright {
+      float: right;
+      margin-left: 24px;
+    }
+
+    .alignleft {
+      float: left;
+      margin-right: 24px;
+    }
+  }
 `;
