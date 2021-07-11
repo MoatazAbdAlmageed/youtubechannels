0562bb32d264f146e47bd9f2cedb5f5f7cc3b767
Moataz Mohammady <moataz@wuilt.com>
2021-07-11 14:12:30 +0200

    remove ReactPixel

--- packages/chakra-theme/src/components/footer.js
+++ packages/chakra-theme/src/components/footer.js
@@ -5,8 +5,6 @@ import { connect, decode } from "frontity";
 import Link from "./link";
 import tags from "./../data/tags.json";
 
-import ReactPixel from "react-facebook-pixel";
-
 const FooterSection = (props) => (
   <Box
     as="footer"
@@ -22,19 +20,6 @@ const FooterSectionItem = (props) => (
 );
 
 const Footer = ({ state }) => {
-  const advancedMatching = { em: "moataz.mohammady@gmail.com" }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
-  const options = {
-    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
-    debug: false, // enable logs
-  };
-  ReactPixel.init("2887412331587526", advancedMatching, options);
-
-  ReactPixel.pageView(); // For tracking page view
-  ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
-  ReactPixel.trackSingle("PixelId", event, data); // For tracking default events.
-  ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
-  ReactPixel.trackSingleCustom("PixelId", event, data); // For tracking custom events.
-
   return (
     <FooterSection alignSelf="flex-end">
       <FooterSectionItem>
